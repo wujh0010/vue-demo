7b0284efed93b38223ff7da7f265d580232877b5
wjhhbu <jianhuawuhbu@163.com>
2020-01-15 22:58:18 +0800

    init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{content}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomePage",
+    data:function () {
+
+      return {
+        content:'主页代码，待添加'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <HomePage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '../components/HomePage'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
